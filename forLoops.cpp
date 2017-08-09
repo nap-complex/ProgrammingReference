10341762c1c2f9380a3c717b2d77f782c520d5f2
John Farrell <nap-complex@users.noreply.github.com>
2017-08-09 18:53:55 -0400

    This is forloop!!!!
    this is a sample for loop in c++ and the first to be added tot he new directory

---
+++ forLoops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  cout << "\n**********************************************\n";
+
+  int i;
+
+  for ( i=1; i<10; i++) {
+     cout << "\nThis is line " << i;
+     }
+
+  cout << "\n**********************************************\n";
+
+
+} // end of main
