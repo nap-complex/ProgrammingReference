2f93db1868b47bb6c2f8b8c653bbaf5b431b650b
emmaDell <virtual.emporer@gmail.com>
2018-02-11 14:29:52 -0500

    updating file handling stuff

---
+++ Python_Reference/1_fileHandlingTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+print("")
+print("")
+print("***************************************")
+print("")
+print("")
+
+print("something is about to happen:")
+
+file1 = open('testfile.txt', 'a')
+file1.write("this should be a line in the file\n")
+file1.close()
+
+
+print("")
+print("")
+print("***************************************")
+print("")
+print("")
