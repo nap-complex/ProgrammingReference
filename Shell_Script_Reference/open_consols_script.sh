b4dcbdb486b670fc7ac23d32bc05b04e49044b55
jlf <virtual.emporer@gmail.com>
2018-02-24 00:43:39 -0500

    adding the shell script practice

---
+++ Shell_Script_Reference/open_consols_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+gnome-terminal -e 'sh -c "cd /media/jlf/2ndDiskPart1/Documents/Programming\ Master\ 20171203/gitHub2\ 20171203/ProgrammingReference/HTML_Reference/Online\ webpage\ making\ practice\ notes\ 20180121; ls -l; exec bash;"' --geometry=117x68+0+0
+
+gnome-terminal -e 'sh -c "cd /media/jlf/2ndDiskPart1/Documents/Programming\ Master\ 20171203/gitHub2\ 20171203/ProgrammingReference/HTML_Reference/Online\ webpage\ making\ practice\ notes\ 20180121; ls -l; exec bash;"' --geometry=117x68+1000+0
+
+
+
