e8cb0614dd152866cacaeece3257f157f55d10d7
jlf <virtual.emporer@gmail.com>
2018-02-27 00:53:58 -0500

    webpages 3 mostly done

--- Shell_Script_Reference/open_consols_script.sh
+++ Shell_Script_Reference/open_consols_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-gnome-terminal -e 'sh -c "cd /media/jlf/2ndDiskPart1/Documents/Programming\ Master\ 20171203/gitHub2\ 20171203/ProgrammingReference/HTML_Reference/Online\ webpage\ making\ practice\ notes\ 20180121; ls -l; exec bash;"' --geometry=117x68+0+0
+gnome-terminal -e 'sh -c "cd /media/jlf/2ndDiskPart1/Documents/Programming\ Master\ 20171203/gitHub2\ 20171203/ProgrammingReference/HTML_Reference/Online\ webpage\ making\ practice\ notes\ 20180121/1_w3schools_practice_html/1st_practice_webpage; ls -l; exec bash;"' --geometry=117x68+0+0
 
-gnome-terminal -e 'sh -c "cd /media/jlf/2ndDiskPart1/Documents/Programming\ Master\ 20171203/gitHub2\ 20171203/ProgrammingReference/HTML_Reference/Online\ webpage\ making\ practice\ notes\ 20180121; ls -l; exec bash;"' --geometry=117x68+1000+0
 
+gnome-terminal -e 'sh -c "cd /media/jlf/2ndDiskPart1/Documents/Programming\ Master\ 20171203/gitHub2\ 20171203/ProgrammingReference/HTML_Reference/Online\ webpage\ making\ practice\ notes\ 20180121/1_w3schools_practice_html/1st_practice_webpage; ls -l; exec bash;"' --geometry=117x68+1000+0
 
 
