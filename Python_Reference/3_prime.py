6f442f0038fe987b8d29c61791a997c984bbfd86
jlf_mac <jlf.inbox@icloud.com>
2018-04-16 16:24:54 -0400

    python stuff agin

---
+++ Python_Reference/3_prime.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import math
+
+
+# ------------------------------------------------------------------------------------
+
+def makeLine():
+	print('')
+	print('************************************************************')
+	print('')
+
+# ------------------------------------------------------------------------------------
+
+def isItPrime( startNum ):
+
+	maxTestNum = int(math.ceil(math.sqrt(startNum)))
+	
+	if ( startNum % 2 == 0 ):
+		return(0)
+
+	cnt1 = 3
+	while ( cnt1 <= maxTestNum ):
+		if ( startNum % cnt1 == 0 ):
+			return(0)
+		cnt1 += 2
+	return(1)
+
+# ------------------------------------------------------------------------------------
+ 
+def main():
+
+	makeLine()
+
+	startNum = 3
+	if ( startNum % 2 == 0 ):
+		startNum += 1
+
+	maxTest = startNum + 100
+
+	print('startNum => ' + '{:,}'.format(startNum))
+	print('maxTest  => ' + '{:,}'.format(maxTest))
+
+	print('')
+	print('')
+	
+	while ( startNum < maxTest ): 
+		if ( isItPrime(startNum) == 1 ):
+			print('Prime Number => ' + '{:,}'.format(startNum))
+		startNum += 2;
+
+	makeLine()
+
+# ------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+	main()
+
+# ------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------
+
---
+++ Python_Reference/5_avoid_newline.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+print('')
+print('--------------------------------------------')
+print('')
+
+
+print('There is a new line:')
+
+print('There is not', end='')
+print(' a new line')
+
+
+
+print('')
+print('--------------------------------------------')
+print('')
+
