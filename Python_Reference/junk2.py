f18017c378662ce77a9faafe02393b4b34a875f1
jlf_mac <jlf.inbox@icloud.com>
2018-04-17 00:30:56 -0400

    some python html page stuff

---
+++ Python_Reference/junk2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+def makeLine():
+	print()
+	print("*********************************************************")
+	print()
+
+
+
+
+
+a = [ 1, 2, 3, 4]
+b = [ "cow", 7, "g", 8.8 ]
+
+makeLine()
+
+for i in range(4):
+	print(a[i], end=" ")
+
+print()
+print()
+
+for i in range(len(b)):
+	print(b[i])
+
+print()
+
+makeLine()
+
+
