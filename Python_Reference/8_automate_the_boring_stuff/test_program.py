921fb0f7a176a5602e0390243d692c530649b639
jlf_mac <jlf.inbox@icloud.com>
2018-06-16 01:11:07 -0400

    some stuff

--- Python_Reference/8_automate_the_boring_stuff/test_program.py
+++ Python_Reference/8_automate_the_boring_stuff/test_program.py
@@ -1,46 +1,77 @@
 #! /usr/bin/env python3
 
-def whatThe():
-	print("\n")
-	print("-----------------------------------------------------------")
-	print("in the def 'whatThe'")
-	print("-----------------------------------------------------------")
+def while_loop_thing():
 
+	print("\n***** Beginning of while_loop_thing() *****\n")
+
+	spam = 0
+	while spam < 5:
+		print(str(spam) + " is this line")
+		spam += 1
+
+	print("\n***** Beginning of while_loop_thing() *****\n")
+
+def for_loop_thing():
+
+	print("\n***** Beginning of for_loop_thing() *****\n")
+	
+	name = "Indicator"
+
+	for x in name:
+		print(x)
+
+	print("\n***** End of for_loop_thing() *****\n")
+
+def if_then_else_elif():
+
+	print("\n***** Begining of if_then_else_elif() *****\n")
+
+	name = "Alice"
+
+	print("name = " + name)
+
+	if name == 'Alice':
+		print("Hi, Alice!")
+	else:
+		print("Not that person")
+
+	# how do you comment in this?
+	if name == 'Bob':
+		print("Hi, Alice!")
+	else:
+		print("Not that person")
+
+	name = 'oooh'
+
+	if name == 'ooo':
+		print("Hi, Alice!")
+	elif name == 'oooh':
+		print("that was 'oooh'")
+
+	print()
+
+	for x in range(10):
+		if x == 5:
+			print(str(x) + " is my favorite number")
+		elif ( x % 2 == 0 ):
+			print(str(x) + " is an even number")
+		else:
+			print(str(x) + " is an odd number")
+
+	print("\n***** End of if_then_else_elif() *****\n")
+
+# -------------------------------------------------------------------------------------------------
+
+for i in range(20):
+	print()
 
 print("\n\n*************** START PROGRAM *******************")
 print("*************************************************\n\n")
 
-print("Did something happen?")
+# if_then_else_elif()
+	
+# for_loop_thing()
 
-name = "Alice"
-
-print("name = " + name)
-
-if name == 'Alice':
-	print("Hi, Alice!")
-else:
-	print("Not that person")
-
-
-if name == 'Bob':
-	print("Hi, Alice!")
-else:
-	print("Not that person")
-
-name = 'oooh'
-
-if name == 'ooo':
-	print("Hi, Alice!")
-elif name == 'oooh':
-	print("that was 'oooh'")
-
-whatThe()
-
-print("\n\n-------------------------------------------------\n\n")
-
-for x in name:
-	print(x)
-
-print("\n\n-------------------------------------------------\n\n")
+while_loop_thing()
 
 print("\n\n*************************************************\n\n")
