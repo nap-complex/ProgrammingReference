8204242a87d2dca76add5e136117c2d2a36ea89c
jlf <virtual.emporer@gmail.com>
2019-01-07 20:30:07 -0500

    more euler problem 3 stuff

--- Project_Euler_Website_Problems/3_Problem_3_Largest_Prime_Factor/4_factoring_practice.cpp
+++ Project_Euler_Website_Problems/3_Problem_3_Largest_Prime_Factor/4_factoring_practice.cpp
@@ -1,77 +1,116 @@
 #include <iostream>
 #include <vector>
-#include <climits>
 #include <cmath>
 using namespace std;
 
 /*
 
-	Just a prime number program
 	Jan 7, 2019 -  JLF
 
+	Largest prime factor - project euler problem 3
+
+  	The prime factors of 13195 are 5, 7, 13 and 29.
+
+	What is the largest prime factor of the number 600851475143 ?
 
 */
 
 // prototypes
 
 bool isItPrime(long);
+long makeTheNextPrime(long); 
 void makeLine();
 
 // ------------------------------------------------------------------------------------------------------------------
 
 int main() {
 
-	makeLine();
+	makeLine();   // this is here to format output for readability
 
 	long inputNumber = 600851475143;
+//	long inputNumber = 13195;              // test number
+//	long inputNumber = 1234567890;         // test number
+//	long inputNumber = 17;                 // test number
 	long targetNumber = inputNumber;
-	long shortInputNumber = 600;
-	long highNum;
 	long ct1;
-	long primeCount = 0;
 	vector<long> factors;
 	bool wasItPrime;
+	long largestPrimeFactor = 0;
 
 	// ----------------------------------------------------------------------------	
+
+	// The introduction in the output
+	cout << "\nWe will now find the prime factors of " << inputNumber;
+
+	// ----------------------------------------------------------------------------	
+
+
 	// check to see if the inputNumber is a prime number
-	cout << "input number, was it prime?" << endl;
+	// this is probably not needed, but is here in case the source of
+	// the input number is jerking around with us
 	if ( isItPrime(inputNumber) ) {
-		cout << "Yes it was!";
 		wasItPrime = true;
+		cout << "\nThe test number itself was a prime number.  Done. ";
 	} else {
-		cout << "No it wasn't!";
 		wasItPrime = false;
 	}
+	
 	// ----------------------------------------------------------------------------	
+
 	// if inputNumber was not a prime number... staaaaaart factoring
 	if ( !wasItPrime ) {
-
-		cout << "\nI guess it wasn't prime!";
 		
-		ct1 = 2;
+		ct1 = 2;  // this is the prime number for testing the target number
 
-		
+		// targetNumber will be "1" when it has been completely factored, and we're done
+		while ( targetNumber != 1 ) {
 
-	}
+			// if ct1 is a factor of targetNumber, add ct1 to the list of factors, then
+			// test if ct1 will factor into targetNumber again, dividing targetNumber by the
+			// new factor to get the next targetNumber
+			if ( (targetNumber%ct1) == 0 ) {
+				factors.push_back(ct1);
+				targetNumber = targetNumber / ct1;
+			// if ct1 will not factor into target number move on to the next higher prime
+			// and try that
+			} else {
+				ct1 = makeTheNextPrime(ct1);
+			} // end of if else
+
+		} // end of while
+
+	} // end of if ( !wasItPrime ) {
 
 	// ----------------------------------------------------------------------------	
+
+	// display the factors found:
+	cout << "\nThe Prime Factors:";
+	if ( !wasItPrime ) {
+		for ( unsigned long i = 0; i < factors.size(); i++ ) {
+			// this next if statement filters for the largest factor
+			if ( factors[i] > largestPrimeFactor ) { largestPrimeFactor = factors[i]; } 
+			cout << factors[i] << " ";
+		} 
+	} else {
+		cout << "\nThe only prime factors are 1 and the number itself => " << inputNumber;
+	} 
+
 	// ----------------------------------------------------------------------------	
+
+	// but the question was, what is the largest prime factor, so...
+	cout << "\nso... the largest prime factor is => " << largestPrimeFactor;
+
 	// ----------------------------------------------------------------------------	
 
-
-
-	makeLine();
-
-	while ( targetNumber != 0 ) {
-
-
-	} // end of while
-	
+	makeLine();   // this is here to format output for readability
 
 } // end of main
 
-
 // ------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------
+
+// this function takes as input a number and returns true if that number if prime and false if it is not
+// We are ignoring the number 2 being prime, so don't test for that
 bool isItPrime(long num) {
 
 	long highestTestNumber;
@@ -95,13 +134,31 @@ bool isItPrime(long num) {
 	return true;
 
 } // end of isItPrime
+
 // ------------------------------------------------------------------------------------------------------------------
 
-
+// this function makes a line accross the output screen purely for format readability
 void makeLine() {
 	cout << "\n\n---------------------------------------------------------------------------------\n\n";
 } // end of makeLine
+
 // ------------------------------------------------------------------------------------------------------------------
+
+// this function takes as input a prime number ( hopefully, there's no error checking ) and returns the next higher prime number
+long makeTheNextPrime(long num) {
+
+	if ( num == 2 ) return 3;
+
+	num = num + 2;
+
+	while ( !isItPrime(num) ) {
+		num = num + 2;
+	} // end of while
+
+	return num;
+
+} // end of makeTheNextPrime
+
 // ------------------------------------------------------------------------------------------------------------------
 
 
