f40ce33bc43f340aa76af1b10f26c499b6c3510c
jlf_mac <jlf.inbox@icloud.com>
2019-02-19 16:17:42 -0500

    euler annoyances

--- E_Project_Euler_Website_Problems/023_Problem_23_Non-Abundant_Sums/2_Problem_23_Non-abundant_sums.cpp
+++ E_Project_Euler_Website_Problems/023_Problem_23_Non-Abundant_Sums/2_Problem_23_Non-abundant_sums.cpp
@@ -4,30 +4,6 @@ using namespace std;
    Jan 27, 2019  -  JLF
 
    Non-abundant sums
-   Problem 23
-
-   A perfect number is a number for which the sum of its proper divisors is exactly 
-   equal to the number. For example, the sum of the proper divisors of 28 would 
-   be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
-
-   A number n is called deficient if the sum of its proper divisors is less than n 
-   and it is called abundant if this sum exceeds n.
-
-   As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number 
-   that can be written as the sum of two abundant numbers is 24. By mathematical analysis, 
-   it can be shown that all integers greater than 28123 can be written as the sum of two 
-   abundant numbers. However, this upper limit cannot be reduced any further by analysis 
-   even though it is known that the greatest number that cannot be expressed as the sum 
-   of two abundant numbers is less than this limit.
-
-   Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
-
-   thinkin' =>
-
-   1. find the proper divisors of a number
-	a. 1 is always a proper divisor, so just start with that and then:
-	b. find the next proper devisor
-
 */
 
 // ---------------------------------------------------------------------------------------------------------
@@ -47,16 +23,7 @@ int main(){
 	int totalSumThing;
 	
 	cout << "nothing";
-	totalSumThing = 0;
-	
-	for ( testNumber = 1; testNumber < 28; testNumber++ ) {
-		cout << "something";
-//	for ( testNumber = 1; testNumber < 28123 + 1; testNumber++ ) {
-		if (!isItTheSumOf2AbundantNumbers(testNumber)){
-			cout << testNumber << ".";
-			totalSumThing = totalSumThing + testNumber;
-		} else cout << "not "; 
-	}
+	totalSumThing = 0;	
 
 	cout << "\nSum of non-thing numbers => " << totalSumThing;
 
