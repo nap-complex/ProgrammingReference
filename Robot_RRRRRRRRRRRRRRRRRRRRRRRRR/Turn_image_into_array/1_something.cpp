aafc807d42ace4e24153758c7006be39ee2f51d9
jlf <virtual.emporer@gmail.com>
2019-05-22 21:45:10 -0400

    why did this not happen already?

---
+++ Robot_RRRRRRRRRRRRRRRRRRRRRRRRR/Turn_image_into_array/1_something.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+
+	cout << "\n***************************************************\n";
+
+	cout << "\nIs this a program?\n";
+
+	cout << "\n***************************************************\n";
+
+	return 0;
+}
