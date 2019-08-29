8ac41bf5bd07882c7705d1703a44f7b47339c90d
jlf <virtual.emporer@gmail.com>
2019-08-29 19:42:26 -0400

    whatthefuck? Over?

---
+++ Websites/junk_file/wget_loop.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+//
+// Put the date and your inits here
+//
+// Write a description of the program here
+//
+// ---------------------------------------------------
+
+def makeLine():
+    print("***************************************")
+
+
+def helloWorld():
+    print("Hello World!")
+
+
+// ---------------------------------------------------
+
+def main():
+    makeLine()
+
+    helloWorld()
+
+    makeLine()
+
+
+// ---------------------------------------------------
+
+if __name__ == "__main__":
+    main()
